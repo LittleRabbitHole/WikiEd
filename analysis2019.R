8e72f7eacf89a85667f787836dc9d7101da6117f
Angrrr_ <li.ang198744@gmail.com>
2019-01-26 15:10:04 -0500

    Update analysis2019.R

--- analysis2019.R
+++ analysis2019.R
@@ -30,8 +30,8 @@ user_data$article_edits_log= log(user_data$article_count + 0.1)
 
 medfit <- lmer(article_edits_log ~ indiv_group + control_wikied + class_size_log + (1|courseID), data = user_data)
 medfit <- lmer(article_edits_log ~ indiv_group  + class_size_log + (1|courseID), data = user_data)
-summary(med1.fit)
-ls_means(medfit)
+summary(medfit)
+ls_means(medfit, pairwise=TRUE)
 
 # (group > individual/control, control < indiv/group)
 
@@ -45,6 +45,48 @@ model2a1 <- coxph(SurvObj ~ article_edit_log + talk_count_log + usertalk_count_l
                   data = user_data)
 summary(model2a1) 
 AIC(model2a1) 
+
+
+####after semester#######
+user_data = read.csv("afterSocialization_effort_retention.csv")
+colnames(user_data)
+user_data[is.na(user_data)] <- 0
+
+user_data$control_wikied = as.factor(user_data$control_wikied)
+user_data$indiv_group = as.factor(user_data$indiv_group)
+
+user_data$class_size_log= log(user_data$class_size + 0.1)
+user_data$article_edit_log= log(user_data$article_count + 0.1)
+user_data$talk_count_log= log(user_data$talk_count + 0.1) 
+user_data$usertalk_count_log= log(user_data$usertalk_count + 0.1)
+user_data$unique_articles_log= log(user_data$unique_article_numbers + 0.1)
+user_data$user_count_log= log(user_data$user_count + 0.1)
+user_data$ave_sizediff_norm= scale(user_data$ave_sizediff,center = TRUE, scale = TRUE)
+user_data$SurvObj <- with(user_data, Surv(dayindex, death == 1))
+
+user_data$article_edits_log= log(user_data$article_count + 0.1)
+
+##effort##
+
+med1.fit <- lmer(article_edits_log ~ indiv_group + control_wikied + class_size_log + (1|courseID), data = user_data)
+summary(med1.fit)
+medfit <- lmer(article_edits_log ~ indiv_group  + class_size_log + (1|courseID), data = user_data)
+ls_means(medfit, pairwise=TRUE)
+
+# (control < indiv/group)
+
+##retention##
+model2a1 <- coxph(SurvObj ~ article_edit_log + talk_count_log + usertalk_count_log
+                  + user_count_log + unique_articles_log +class_size_log + ave_sizediff_norm
+                  + control_wikied  
+                  + indiv_group
+                  + cluster(courseID),
+                  #ties = "breslow",
+                  data = user_data)
+summary(model2a1) 
+AIC(model2a1) 
+
+
 
 ####only students interaction####
 user_data = read.csv("duringSocializationCommunication.csv")
@@ -80,7 +122,7 @@ length(unique(user_data$courseID))
 
 model2a1 <- coxph(SurvObj ~ #reach_out_stu_log + reach_out_wiki_log + reach_in_stu_log + reach_in_wiki_log
                     + group_group 
-                    + score_diff
+                  + score_diff
                   + cluster(courseID)
                   + article_edit_log + talk_count_log +usertalk_count_log+ user_count_log+unique_articles_log + ave_sizediff_norm + student_count_log, 
                   data = user_data)
@@ -90,47 +132,10 @@ summary(model2a1)
 model2a2 <- coxph(SurvObj ~ reach_out_stu_log + reach_out_wiki_log + reach_in_stu_log + reach_in_wiki_log
                   + group_group 
                   + score_diff
-                  + cluster(courseID)
-                  + article_edit_log + talk_count_log +usertalk_count_log+ user_count_log+unique_articles_log + ave_sizediff_norm + student_count_log, 
+                  + article_edit_log + talk_count_log +usertalk_count_log+ user_count_log+unique_articles_log + ave_sizediff_norm + student_count_log
+                  + cluster(courseID), 
                   data = user_data)
 summary(model2a2) #0.029
 AIC(model2a) #286720
 
 
-
-####after semester#######
-user_data = read.csv("afterSocialization_effort_retention.csv")
-colnames(user_data)
-user_data[is.na(user_data)] <- 0
-
-user_data$control_wikied = as.factor(user_data$control_wikied)
-user_data$indiv_group = as.factor(user_data$indiv_group)
-
-user_data$class_size_log= log(user_data$class_size + 0.1)
-user_data$article_edit_log= log(user_data$article_count + 0.1)
-user_data$talk_count_log= log(user_data$talk_count + 0.1) 
-user_data$usertalk_count_log= log(user_data$usertalk_count + 0.1)
-user_data$unique_articles_log= log(user_data$unique_article_numbers + 0.1)
-user_data$user_count_log= log(user_data$user_count + 0.1)
-user_data$ave_sizediff_norm= scale(user_data$ave_sizediff,center = TRUE, scale = TRUE)
-user_data$SurvObj <- with(user_data, Surv(dayindex, death == 1))
-
-user_data$article_edits_log= log(user_data$article_count + 0.1)
-
-##effort##
-
-med1.fit <- lmer(article_edits_log ~ indiv_group + control_wikied + class_size_log + (1|courseID), data = user_data)
-summary(med1.fit)
-
-# (control < indiv/group)
-
-##retention##
-model2a1 <- coxph(SurvObj ~ article_edit_log + talk_count_log + usertalk_count_log
-                  + user_count_log + unique_articles_log +class_size_log + ave_sizediff_norm
-                  + control_wikied  
-                  + indiv_group,
-                  #+ cluster(courseID),
-                  #ties = "breslow",
-                  data = user_data)
-summary(model2a1) 
-AIC(model2a1) 
