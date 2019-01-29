9fa8dd6f2f942657784f9bb7318dd4d0e0a04d3d
Angrrr_ <li.ang198744@gmail.com>
2019-01-29 16:57:17 -0500

    commit

--- studentTalkPage_collection.py
+++ studentTalkPage_collection.py
@@ -31,6 +31,7 @@ import os
 import pandas as pd
 import numpy as np
 from utilities import GetPageRevision
+from data_processor import Today, TimeDelta
 
 # read the list of users
 
@@ -51,202 +52,179 @@ def userList():
     
     return studentList
 
-#########################################
-###edit in to students' user talk page
-dir_file = "/Users/angli/ANG/OneDrive/Documents/Pitt_PhD/ResearchProjects/Wiki_Edu_Project/Data/finalRevise/"
-csv_f = csv.writer(open(dir_file+"students_talkpage_contribution.csv", "w", encoding="UTF-8"))
-#write first row
-csv_f.writerow(['wpid','startdate','usertalkpage','talkpage_editor', 'timestamp', 'size','comment'])
-#write error
 
-
-#student lists
-studentList = userList()
-
-# read username catch the Wiki data
-n=0
-for student in studentList:
-    wpid = student[0]
-    startdate = student[1]
-    wpid = wpid.strip()
-    #print (rawID)
-    #wpid_nospace = wpid.replace(" ","_")
-    #decode student's name into ascii
-    decode_wpid = urllib.parse.quote(wpid)
-    #api
-    #api_call = ("https://en.wikipedia.org/w/api.php?action=query&list=usercontribs&ucuser={}&ucdir=newer&ucstart={}&ucend={}&ucprop=title|timestamp|comment|size|sizediff|flags&uclimit=500&format=json").format(decode_wpid,starttime,endtime)#Kingsleyta
-    #https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=User_talk:Kingsleyta&rvdir=newer&rvend=2015-03-30T13:20:52Z&rvprop=timestamp|user|size|comment&rvdir=newer&rvlimit=500    
-    api_call = ("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=User talk:{}&rvdir=newer&rvprop=timestamp|user|size|comment&rvdir=newer&rvlimit=500&format=json").format(decode_wpid)#Kingsleyta
-    try: 
-        talkpage_revisions_lst = GetPageRevision(api_call)
-        for feature in talkpage_revisions_lst:
-            if feature.get('user') != wpid: #not writeout self editing
-                csv_f.writerow([ wpid, startdate, "user_talk", feature.get('user'), feature['timestamp'], feature.get('size'), feature.get('commet')])
-    except KeyError:
-        csv_f.writerow([wpid, startdate, "user_talk", "no user talk", "",0,""])
-    n+=1
-    if n%100==0:
-        print (n) 
-
-
-
-##########################################
-### collect student contribution in user talk page (edit out)
-
-csv_f = csv.writer(open("studentContri_talkout_2015-2016_0306.csv", "w"))
-#write first row
-csv_f.writerow(['wpid','SID','course','courseID','oldStudent','article','ns','page_title','talkpage_owner', 'timestamp', 'size','comment'])
-#write error
-csv_error = csv.writer(open("studentsContri_talkout_errorusers_20152016_0306.csv", "w", encoding="UTF-8"))
-
-#user talk page revisions
-#regular expression to match out the username
-import re
-talkpageowner_re = re.compile(r'User talk:(.*)/|User talk:(.*)', re.DOTALL)#User talk:(.*)/sandbox|User talk:(.*)/
-
-n=0
-for line in info[1::]:
-    line = line.strip()
-    info_lst = line.split('\t')
-    #studentID
-    SID = info_lst[1]
-    #course
-    course = info_lst[2]
-    #courseID
-    courseID = info_lst[3]
-    #return Student
-    oldStudent = info_lst[4]
-    #get name from names list
-    wpid =  info_lst[0]
-    wpid = wpid.strip()
-    #print (rawID)
-    wpid_nospace = wpid.replace(" ","_")
-    #decode student's name into ascii
-    decode_wpid = urllib.parse.quote(wpid_nospace)
-    #quarter time window
-    starttime = info_lst[-2]+"T00:00:00Z"
-    endtime = info_lst[-1]+"T11:59:59Z"
+def usernamesLst():
+    dir_file = "/Users/angli/ANG/OneDrive/Documents/Pitt_PhD/ResearchProjects/Wiki_Edu_Project/Data/finalRevise/"
+    filename = "control_students_all.csv"
     
-    #API https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=User_talk:Averysf&rvuser=Poetries&rvdir=newer&rvprop=timestamp|user|size|comment&rvdir=newer&rvlimit=500
-    api_call = ("https://en.wikipedia.org/w/api.php?action=query&list=usercontribs&ucuser={}&ucnamespace=3&ucdir=newer&ucstart={}&ucend={}&ucprop=timestamp|title|sizediff|comment&uclimit=500&format=json").format(decode_wpid,starttime,endtime)#Kingsleyta
-    response=urllib.request.urlopen(api_call)
-    str_response=response.read().decode('utf-8')
-    responsedata = json.loads(str_response)
-    try: 
-        revision_data_lst=responsedata["query"]["usercontribs"]#list
-        for feature in revision_data_lst:
-            talk_page_title = feature.get('title')
-            talk_page_owner1 = re.match(talkpageowner_re, talk_page_title).group(1)
-            talk_page_owner2 = re.match(talkpageowner_re, talk_page_title).group(2)
-            talk_page_owner_lst=list(set([talk_page_owner1,talk_page_owner2]))
-            talk_page_owner = [x for x in talk_page_owner_lst if x is not None][0]
-            if wpid != talk_page_owner: #not edit in their own talk page
-                csv_f.writerow([wpid, SID,course,courseID,oldStudent,"user_talk", 3, talk_page_title, talk_page_owner, feature['timestamp'], feature.get('sizediff'), feature.get('comment')])
-    except KeyError:
-        #pass
-        csv_error.writerow([wpid, SID,course,courseID,oldStudent,"user_talk", 3, page_title, "no user talk", "no user talk","no user talk","no user talk"])
-    n+=1    
-    if n%100==0:
-        print (n) 
+    f = open(dir_file+filename) 
+    students = f.readlines()
+    f.close()
+    
+    studentList = []
+    for student in students[1::]:
+        student = student.strip().split(",")
+        student_username = student[0].strip()
+        #student_startdate = student[2].strip()
+        studentList.append(student_username)
+    
+    return studentList
+
+######edit in to students' user talk page
+
+def talkpageRevisions():
+
+    dir_file = "/Users/angli/ANG/OneDrive/Documents/Pitt_PhD/ResearchProjects/Wiki_Edu_Project/Data/finalRevise/"
+    csv_f = csv.writer(open(dir_file+"students_talkpage_revisions.csv", "w", encoding="UTF-8"))
+    #write first row
+    csv_f.writerow(['wpid','startdate','usertalkpage','talkpage_editor', 'timestamp', 'size','comment'])
+    #write error
+    
+    
+    #student lists
+    studentList = userList()
+    
+    # read username catch the Wiki data
+    n=0
+    for student in studentList:
+        wpid = student[0]
+        startdate = student[1]
+        wpid = wpid.strip()
+        #print (rawID)
+        #wpid_nospace = wpid.replace(" ","_")
+        #decode student's name into ascii
+        decode_wpid = urllib.parse.quote(wpid)
+        #api
+        #api_call = ("https://en.wikipedia.org/w/api.php?action=query&list=usercontribs&ucuser={}&ucdir=newer&ucstart={}&ucend={}&ucprop=title|timestamp|comment|size|sizediff|flags&uclimit=500&format=json").format(decode_wpid,starttime,endtime)#Kingsleyta
+        #https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=User_talk:Kingsleyta&rvdir=newer&rvend=2015-03-30T13:20:52Z&rvprop=timestamp|user|size|comment&rvdir=newer&rvlimit=500    
+        api_call = ("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=User talk:{}&rvdir=newer&rvprop=timestamp|user|size|comment&rvdir=newer&rvlimit=500&format=json").format(decode_wpid)#Kingsleyta
+        try: 
+            talkpage_revisions_lst = GetPageRevision(api_call)
+            for feature in talkpage_revisions_lst:
+                if feature.get('user') != wpid: #not writeout self editing
+                    csv_f.writerow([ wpid, startdate, "user_talk", feature.get('user'), feature['timestamp'], feature.get('size'), feature.get('commet')])
+        except KeyError:
+            csv_f.writerow([wpid, startdate, "user_talk", "no user talk", "",0,""])
+        n+=1
+        if n%100==0:
+            print (n) 
 
 
-#https://en.wikipedia.org/w/api.php?action=query&list=usercontribs&ucuser=Kingsleyta&ucdir=newer&ucnamespace=3&ucprop=title|timestamp|comment|size|sizediff|flags&uclimit=500
-
-#####after manually remove sandbox title
-studentContri_talk = pd.read_csv("studentContri_talkout_2015-2016_0306.csv", encoding = "ISO-8859-1")
-studentContri_talk.columns.values
-drop_index = []
-for index, row in studentContri_talk.iterrows():
-    if row['wpid'] == row['talkpage_owner']:
-        drop_index.append(index)
-
-#drop self editing    
-studentContri_talk = studentContri_talk.drop(studentContri_talk.index[drop_index])
-
-#reset index
-studentContri_talk = studentContri_talk.reset_index(drop=True)
-
-#drop edit classmates
-#leave only wikipedian
-drop_index = []
-for index, row in studentContri_talk.iterrows():
-    if row['talkpage_owner'] in studentList:
-        drop_index.append(index)
-
-studentContri_talk = studentContri_talk.drop(studentContri_talk.index[drop_index])
-studentContri_talk = studentContri_talk.reset_index(drop=True)
-
-studentContri_talk.to_csv("StudentContri_talkout_Wiki_2015-2016_0306.csv",index=False)
-
-#wikipedians students talked to
-wiki_list = list(set(list(studentContri_talk['talkpage_owner'])))
-len(wiki_list) #740 student contribute to 740 wikipedians' talk page
-
-
-
-
-
-
-##useless
-###########################################################
-#during semester
-students_talk = pd.read_csv("students_talk_2015-2016_0306.csv", encoding = "ISO-8859-1")
-students_talk.columns.values
-
-#wikipedians talked to
-talk_pair = students_talk[["wpid","talkpage_user"]]#.drop_duplicates()
-students_rowindex = []
-for i, row in talk_pair.iterrows():
-    if row["talkpage_user"] in studentList:
-        students_rowindex.append(i)
+if __name__ == "__main__":
+    talkpageRevisions()
+    studentscohort = usernamesLst()
+    
+    dir_file = "/Users/angli/ANG/OneDrive/Documents/Pitt_PhD/ResearchProjects/Wiki_Edu_Project/Data/finalRevise/"
+    file = "students_talkpage_revisions.csv"
+    data = pd.read_csv(dir_file+file)
+    
+    data['timestamp'] = pd.to_datetime(data['timestamp'])
+    data['register_date'] = pd.to_datetime(data['register_date'])
+    data["day_index_timedelta"] = data['timestamp'] - data['register_date']#as timedelta
+    data["day_index"] = data["day_index_timedelta"].dt.days
+    data["wiki_msgin"] =  data['talkpage_editor'].apply(lambda x: 0 if x in studentscohort else 1)
+    
+    aggre_data=[]
+    Grouped = data.groupby(['wpid', 'register_date', 'day_index'])
+    #Grouped.ngroups
+    
+    n=0
+    for pidgroup in Grouped:
+        n+=1
+        if n%100==0: print (n)
+        #if n==4: break
+        wpid, register_date, day_index = pidgroup[0]
+        register_date = register_date.strftime('%Y-%m-%d')
         
-talk_pair = talk_pair.drop(talk_pair.index[students_rowindex])  
-talk_pair = talk_pair.drop_duplicates() #talk pair of student and wikipedians     
+        msgin_lst = list(pidgroup[1]["wiki_msgin"])
+        from_wiki = sum(msgin_lst)
+        from_stu = len(msgin_lst) - from_wiki
+        #unit data
+        pidgroup_data = [wpid, register_date, day_index, from_wiki, from_stu]        
+        aggre_data.append(pidgroup_data)
+    
+    #write out
+    i = 0
+    outString = 'wpid,register_date,day_index,from_wiki,from_stu'
+    for item in aggre_data:
+        str_item = [str(i) for i in item]
+        i += 1
+        outString += '\n'
+        outString += ','.join(str_item)
+        
+#    result_path = '{}/results'.format(file_loc)
+#    if not os.path.exists(result_path):
+#        os.makedirs(result_path)
+        
+    with open(dir_file+"studentTalkpage_aggre.csv", 'w') as f:
+        f.write(outString)
+        f.close()
 
-wiki_list = talk_pair["talkpage_user"].tolist() #Wikipedian list student talked to
-user_list = talk_pair["wpid"].tolist() #corresponded student list
+# =============================================================================
+# ##########################################
+# ### collect student contribution in user talk page (edit out)
+# 
+# csv_f = csv.writer(open("studentContri_talkout_2015-2016_0306.csv", "w"))
+# #write first row
+# csv_f.writerow(['wpid','SID','course','courseID','oldStudent','article','ns','page_title','talkpage_owner', 'timestamp', 'size','comment'])
+# #write error
+# csv_error = csv.writer(open("studentsContri_talkout_errorusers_20152016_0306.csv", "w", encoding="UTF-8"))
+# 
+# #user talk page revisions
+# #regular expression to match out the username
+# import re
+# talkpageowner_re = re.compile(r'User talk:(.*)/|User talk:(.*)', re.DOTALL)#User talk:(.*)/sandbox|User talk:(.*)/
+# 
+# n=0
+# for line in info[1::]:
+#     line = line.strip()
+#     info_lst = line.split('\t')
+#     #studentID
+#     SID = info_lst[1]
+#     #course
+#     course = info_lst[2]
+#     #courseID
+#     courseID = info_lst[3]
+#     #return Student
+#     oldStudent = info_lst[4]
+#     #get name from names list
+#     wpid =  info_lst[0]
+#     wpid = wpid.strip()
+#     #print (rawID)
+#     wpid_nospace = wpid.replace(" ","_")
+#     #decode student's name into ascii
+#     decode_wpid = urllib.parse.quote(wpid_nospace)
+#     #quarter time window
+#     starttime = info_lst[-2]+"T00:00:00Z"
+#     endtime = info_lst[-1]+"T11:59:59Z"
+#     
+#     #API https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=User_talk:Averysf&rvuser=Poetries&rvdir=newer&rvprop=timestamp|user|size|comment&rvdir=newer&rvlimit=500
+#     api_call = ("https://en.wikipedia.org/w/api.php?action=query&list=usercontribs&ucuser={}&ucnamespace=3&ucdir=newer&ucstart={}&ucend={}&ucprop=timestamp|title|sizediff|comment&uclimit=500&format=json").format(decode_wpid,starttime,endtime)#Kingsleyta
+#     response=urllib.request.urlopen(api_call)
+#     str_response=response.read().decode('utf-8')
+#     responsedata = json.loads(str_response)
+#     try: 
+#         revision_data_lst=responsedata["query"]["usercontribs"]#list
+#         for feature in revision_data_lst:
+#             talk_page_title = feature.get('title')
+#             talk_page_owner1 = re.match(talkpageowner_re, talk_page_title).group(1)
+#             talk_page_owner2 = re.match(talkpageowner_re, talk_page_title).group(2)
+#             talk_page_owner_lst=list(set([talk_page_owner1,talk_page_owner2]))
+#             talk_page_owner = [x for x in talk_page_owner_lst if x is not None][0]
+#             if wpid != talk_page_owner: #not edit in their own talk page
+#                 csv_f.writerow([wpid, SID,course,courseID,oldStudent,"user_talk", 3, talk_page_title, talk_page_owner, feature['timestamp'], feature.get('sizediff'), feature.get('comment')])
+#     except KeyError:
+#         #pass
+#         csv_error.writerow([wpid, SID,course,courseID,oldStudent,"user_talk", 3, page_title, "no user talk", "no user talk","no user talk","no user talk"])
+#     n+=1    
+#     if n%100==0:
+#         print (n) 
+# 
+# 
+# 
+# 
+# =============================================================================
 
-#collect the wikipedian talk page edited by students
-#Check the user talk page
-csv_f = csv.writer(open("wiki_talk_2015-2016_0306.csv", "w"))
-#write first row
-csv_f.writerow(['wpid_original', 'wikipedian','article','ns','page_title','students', 'timestamp', 'size','comment'])
-
-#user talk page revisions
-
-n=0
-for i in range(len(wiki_list)):
-    wpid_original =  wiki_list[i]
-    sdutent_original = user_list[i]
-    #Wikipedians
-    wpid = wpid_original.strip()
-    wpid_nospace = wpid.replace(" ","_")
-    #article = articles[i]
-    #decode student's name into ascii
-    decode_wpid = urllib.parse.quote(wpid_nospace)
-    #Students
-    studentid = sdutent_original.strip()
-    sid_nospace = studentid .replace(" ","_")
-    #article = articles[i]
-    #decode student's name into ascii
-    decode_sid = urllib.parse.quote(sid_nospace)
-    #API https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=User_talk:Averysf&rvuser=Poetries&rvdir=newer&rvprop=timestamp|user|size|comment&rvdir=newer&rvlimit=500
-    api_call = ("https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=User_talk:{}&rvuser={}&rvdir=newer&rvprop=timestamp|user|size|comment&rvlimit=500&format=json").format(decode_wpid,decode_sid)#Kingsleyta
-    response=urllib.request.urlopen(api_call)
-    str_response=response.read().decode('utf-8')
-    responsedata = json.loads(str_response)
-    page_id = list(responsedata["query"]["pages"].keys())[0]
-    page_title = responsedata['query']['pages'][page_id].get("title")
-    try: 
-        revision_data_lst=responsedata['query']['pages'][page_id]['revisions']
-        for feature in revision_data_lst:
-            csv_f.writerow([wpid_original, wpid, "user_talk", 3, page_title, feature.get('user'), feature['timestamp'], feature.get('size'), feature.get('comment')])
-    except KeyError:
-        pass
-        #csv_f.writerow([wpid_original, wpid, "user_talk", 3, page_title, "no user talk", "no user talk","no user talk","no user talk"])
-    n+=1    
-    if n%100==0:
-        print (n) 
 
 
 
