c9ef9ae67498455e41f685ef183a0dcfbdb61edd
LittleRabbitHole <li.ang198744@gmail.com>
2020-01-06 09:48:49 -0500

    Update CSCW_recheck.py

--- CSCW_recheck.py
+++ CSCW_recheck.py
@@ -26,7 +26,7 @@ def returnJsonCheck(response) -> dict:
         print(response.text)
         sys.exit("json error")
 
-if __name__ == "__main__":
+def checkEdits():
     dir_file = "/Users/jiajunluo/OneDrive/Documents/Pitt_PhD/ResearchProjects/Wiki_Edu_Project/Data/finalRevise/"
     filename = "control_students_all.csv"
         
@@ -61,5 +61,51 @@ if __name__ == "__main__":
         csv_f.writerow([wpid, start_control_wpid, start_total_edits, end_control_wpid, end_total_edits])
     
     f.close()
+  
+#check bots      
+if __name__ == "__main__":
+    dir_file = "/Users/jiajunluo/OneDrive/Documents/Pitt_PhD/ResearchProjects/Wiki_Edu_Project/Data/finalRevise/"
+    filename = "control_students_all_edits.csv"
         
+    f = open(dir_file+filename) 
+    controls = f.readlines()
+    f.close()    
         
+    f = open(dir_file+"control_students_all_edits_bots.csv", "w", encoding="UTF-8")
+    csv_f = csv.writer(f)
+    csv_f.writerow(['wpid','start_control_wpid','edits','start_bot','end_control_wpid','edits','end_bot'])
+
+    n=1
+    for control in controls[1::]:
+        n+=1
+        if n%100==0: print (n)
+        control_lst = control.strip().split(",")
+        wpid = str(control_lst[0])
+        start_control_wpid = str(control_lst[1])
+        start_control_edits = control_lst[2]
+        end_control_wpid = str(control_lst[3])
+        end_control_edits = control_lst[4]
+        
+        start_api_call = "https://en.wikipedia.org/w/api.php?action=query&list=users&ususerids={}&usprop=groups&format=json".format(start_control_wpid)
+        end_api_call = "https://en.wikipedia.org/w/api.php?action=query&list=users&ususerids={}&usprop=groups&format=json".format(end_control_wpid)
+        
+        start_response=requests.get(start_api_call)
+        start_responsedata = returnJsonCheck(start_response) 
+        start_user_group = start_responsedata['query']['users'][0]['groups']
+        if "bot" in start_user_group:
+            start_user_bot = 1
+        else:
+            start_user_bot = 0
+            
+        end_response=requests.get(end_api_call)
+        end_responsedata = returnJsonCheck(end_response) 
+        end_user_group = end_responsedata['query']['users'][0]['groups']
+        if "bot" in end_user_group:
+            end_user_bot = 1
+        else:
+            end_user_bot = 0
+        
+        csv_f.writerow([wpid, start_control_wpid, start_control_edits, start_user_bot, end_control_wpid, end_control_edits, end_user_bot])
+    
+    f.close()
+      
